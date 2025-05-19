f4a099c7a09f9ea2825bbf91bf2b7b9b53f0eebf
TheLazyBlue <kawinter10@gmail.com>
2025-05-18 20:53:13 -0700

    Add GitHub Pages deployment configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/3D-print-gallery-test',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
